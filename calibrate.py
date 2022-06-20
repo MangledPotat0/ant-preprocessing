2e2a9be06161ccfb883e059b1bddd0e4e0a2a94c
maelstrom <cephalopodmaster@protonmail.com>
2022-06-20 21:48:23 +0900

    Initial commit

---
+++ calibrate.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2 as cv
+import glob
+import json
+
+h,w = (7, 7)
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((h*w,3), np.float32)
+objp[:,:2] = np.mgrid[0:w,0:h].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+vid = cv.VideoCapture('debarrel.mp4')
+run, img = vid.read()
+newimg = img
+loops = 0
+ct = 0
+
+while run:
+    loops += 1
+    img = newimg
+    ct = 0
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imwrite('gray.png', gray)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (w,h), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (3,3), (-1,-1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        #cv.drawChessboardCorners(img, (w,h), corners2, ret)
+        #cv.imwrite('img.png', img)
+    while ct < 30:
+        ct += 1
+        run, newimg = vid.read()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints,
+                                            gray.shape[::-1], None, None)
+cv.destroyAllWindows()
+
+ymax, xmax = img.shape[:2]
+print('xmax: ',xmax)
+print('ymax: ',ymax)
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (xmax,ymax), 1,
+                                                 (xmax,ymax))
+
+npzfile = np.savez('calibration_parameters.npz',
+                   mtx = mtx, dist = dist,
+                   newcameramtx = newcameramtx, roi = roi)
+
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png',dst)
+
+# EOF
---
+++ unbarrel.py
@@ -0,0 +1,51 @@
+import argparse as arg
+import numpy as np
+import cv2 as cv
+import glob
+import json
+import os
+import sys
+
+ap = arg.ArgumentParser()
+ap.add_argument('-v', '--video', required = True,
+                help = 'Video files without extension')
+ap.add_argument('-px', '--pixel', required = True,
+                help = 'Pixel resolution')
+ap.add_argument('-f', '--framerate', required = True,
+                help = 'framerate')
+args = vars(ap.parse_args())
+filenames = args['video']
+filenames = glob.glob(filenames)
+
+with np.load('calibration_parameters_{}px.npz'.format(args['pixel'])) as values:
+    for fname in filenames:
+        filename = os.path.splitext(fname)[0]
+        vidfile = cv.VideoCapture('{}.mp4'.format(filename))
+        readable, frame = vidfile.read()
+        mtx = values['mtx']
+        dist = values['dist']
+        newcameramtx = values['newcameramtx']
+        roi = values['roi']
+        x1, y1, x2, y2 = roi
+
+        fourcc = cv.VideoWriter_fourcc(*'mp4v')
+        api = cv.CAP_ANY
+        out = cv.VideoWriter(filename = '{}corrected.mp4'.format(filename),
+                             apiPreference = api,
+                             fourcc = fourcc,
+                             fps = int(args['framerate']),
+                             frameSize = (x2, y2),
+                             isColor = True)
+        
+        while readable:
+            dst = cv.undistort(frame, mtx, dist, None, newcameramtx)
+            dst = dst[y1:y1+y2, x1:x1+x2]
+            out.write(dst)
+            readable, frame = vidfile.read()
+
+    vidfile.release()
+    out.release()
+    cv.destroyAllWindows()
+sys.exit(0)
+
+# EOF
